aea33ab43e4506a089e1129263d62c95dc69ea2c
lucas <lucasgominhaa448@gmail.com>
2022-06-24 11:45:09 -0300

    added study about simple routes in angular (using the module of routes routing module)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'courses', component: CoursesComponent},
+  {path: 'courses'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
