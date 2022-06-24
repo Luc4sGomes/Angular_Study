a58d43af4bf2958583401eefbba5976192a5b8ea
lucas <lucasgominhaa448@gmail.com>
2022-06-24 16:13:55 -0300

    added study router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'courses', component: CoursesComponent},
-  {path: 'login', component: LoginComponent}
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'courses', component: CoursesComponent }
 ];
 
 @NgModule({
