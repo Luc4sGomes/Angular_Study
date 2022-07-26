79129a1112d1fc81f34d4a07f8c7a4d346b9631d
lucas <lucasgominhaa448@gmail.com>
2022-07-26 11:40:59 -0300

    added study about queryParams in Angular

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CoursesService } from './courses.service';
 
 @Component({
@@ -9,13 +11,29 @@ import { CoursesService } from './courses.service';
 export class CoursesComponent implements OnInit {
 
   courses: any[] = [];
+  page!: number;
+  inscription!: Subscription;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService, private route: ActivatedRoute, private router: Router) {
 
   }
 
   ngOnInit(): void {
     this.courses = this.coursesService.getCourses();
+    this.inscription = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.page = queryParams['page'];
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.inscription.unsubscribe();
+  }
+
+  nextPage(): void {
+    this.router.navigate(['/courses'],
+    {queryParams: {'page': ++this.page}});
   }
 
 }
